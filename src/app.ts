0bd2a0828e0dffc67954fa7e3d8e34e692117b23
Lautaro Robles <lrobles@frba.utn.edu.ar>
2024-06-06 20:41:05 -0300

    Simplifico app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,30 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import { createScene } from "./scene";
-import { Engine, Scene } from "@babylonjs/core";
+import { Engine } from "@babylonjs/core";
 
-export class App {
-    // Para que se renderize algo en pantalla
-    // en babylon minimo tiene que existir un Engine y una Scene
-    // TODO: Revisar como usar multiples escenas
-    engine: Engine;
-    scene: Scene;
+// Determina si en la escena se mostrara un menu de debug
+const debug = false
 
-    constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new Engine(canvas)
-        window.addEventListener('resize', () => {
-            this.engine.resize();
-        });
-        this.scene = createScene(this.engine, this.canvas)
-    }
-
-    debug(debugOn: boolean = true) {
-        if (debugOn) {
-            this.scene.debugLayer.show({ overlay: true });
-        } else {
-            this.scene.debugLayer.hide();
-        }
-    }
-
-    run() {
-        this.debug(true);
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
-        });
-    }
-}
-
-console.log(`main.ts starting ${App.name}`);
+// Inicializamos Babylon
+console.log(`Iniciando Juego`);
 window.addEventListener('DOMContentLoaded', () => {
-    let canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-    let app = new App(canvas);
-    app.run();
+    const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement
+    const engine = new Engine(canvas)
+    const scene = createScene(engine, canvas) // Por ahora solamente tenemos una unica escena
+
+    if (debug) {
+        scene.debugLayer.show({ overlay: true });
+    } else {
+        scene.debugLayer.hide();
+    }
+
+    window.addEventListener('resize', () => {
+        engine.resize()
+    })
+
+    engine.runRenderLoop(() => {
+        scene.render()
+    })
+
 });
